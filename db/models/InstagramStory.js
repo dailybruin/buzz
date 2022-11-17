85f7ed27d71e5231ffbe71a0f505dd4520538d09
RandomY-2 <69881587+RandomY-2@users.noreply.github.com>
2022-11-16 18:27:13 -0800

    Remove ```expirationAt``` for database entries

--- db/models/InstagramStory.js
+++ db/models/InstagramStory.js
@@ -6,16 +6,16 @@ const instagramStorySchema = mongoose.Schema({
   figma: { type: String },
   caption: { type: String },
   date: { type: Date },
-  expireAt: {
-    type: Date,
-    default: function () {
-      const now = new Date();
-      now.setDate(now.getDate() + EXPIRATION)
-      return now;
-    }
-  }
+  // expireAt: {
+  //   type: Date,
+  //   default: function () {
+  //     const now = new Date();
+  //     now.setDate(now.getDate() + EXPIRATION)
+  //     return now;
+  //   }
+  // }
 }, { timestamps: true, strict: false });
-instagramStorySchema.index({ expireAt: 1 }, { expireAfterSeconds: 0 });
+// instagramStorySchema.index({ expireAt: 1 }, { expireAfterSeconds: 0 });
 
 let InstagramStory = mongoose.model('InstagramStory', instagramStorySchema);
 module.exports = InstagramStory;
--- db/models/Modular.js
+++ db/models/Modular.js
@@ -4,16 +4,16 @@ const EXPIRATION = 2;
 
 const modularSchema = mongoose.Schema({
   date: { type: Date },
-  expireAt: {
-    type: Date,
-    default: function () {
-      const now = new Date();
-      now.setDate(now.getDate() + EXPIRATION)
-      return now;
-    }
-  }
+  // expireAt: {
+  //   type: Date,
+  //   default: function () {
+  //     const now = new Date();
+  //     now.setDate(now.getDate() + EXPIRATION)
+  //     return now;
+  //   }
+  // }
 }, { timestamps: true, strict: false });
-modularSchema.index({ expireAt: 1 }, { expireAfterSeconds: 0 });
+// modularSchema.index({ expireAt: 1 }, { expireAfterSeconds: 0 });
 
 let Modular = mongoose.model('Modular', modularSchema);
 
