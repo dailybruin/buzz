8a1ee1b192313205ec32ef2f0d215b44fab10f21
Sunny Li <jinghengsunny@gmail.com>
2023-01-31 19:56:51 -0800

    breadcrumbs for menu

--- src/components/Shared/Nav.js
+++ src/components/Shared/Nav.js
@@ -1,10 +1,23 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { Breadcrumbs } from '@mui/material';
 
-export const Nav = () => (<nav>
-  <NavLink exact to="/">Home</NavLink>
-  <NavLink to="/photo">Photo</NavLink>
-  <NavLink to="/staff">Staff</NavLink>
-  <NavLink to="/linebreak">Linebreak</NavLink>
-  <NavLink to="/filler">Filler Text</NavLink>
-</nav>);
+// export const Nav = () => (<nav>
+//   <NavLink exact to="/">Home</NavLink>
+//   <NavLink to="/photo">Photo</NavLink>
+//   <NavLink to="/staff">Staff</NavLink>
+//   <NavLink to="/linebreak">Linebreak</NavLink>
+//   <NavLink to="/filler">Filler Text</NavLink>
+// </nav>);
+
+export const Nav = () => (
+  <Breadcrumbs aria-label="breadcrumb">
+    <NavLink exact to="/">Home</NavLink>
+    <NavLink to="/photo">Photo</NavLink>
+    <NavLink to="/staff">Staff</NavLink>
+    <NavLink to="/linebreak">Linebreak</NavLink>
+    <NavLink to="/filler">Filler Text</NavLink>
+
+  </Breadcrumbs>
+
+);
