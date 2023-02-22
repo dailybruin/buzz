f269ad324b494f1c74c8a95b1bf4c1f2b524c180
Juliet Zhang <julietzhang3@gmail.com>
2023-02-21 19:32:55 -0800

    Replace Design Notes form with MUI TO FIX: htmlFor currently does not work. When a form is submitted, the text does not appear in the table.

--- src/components/Home/DesignNotes/Form.js
+++ src/components/Home/DesignNotes/Form.js
@@ -3,19 +3,33 @@ import AnimateHeight from "react-animate-height";
 import { Formik, Field, Form as FormikForm, ErrorMessage } from 'formik';
 import { postDesignNote } from '../../../services/api';
 import config from "../../../config";
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Box from "@mui/material/Box";
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import "./style.css";
 
 export class DesignNotesForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      open: false
+      open: false,
+      artStatus: '',
     };
     this.submitNote = this.submitNote.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   submitNote(values) {
     const req = Object.assign({}, values, { "section": this.props.section });
     return postDesignNote(this.props.date, req);
+  }
+
+  handleChange(event) {
+    this.setState({ artStatus: event.target.value });
   }
 
   render() {
@@ -35,7 +49,7 @@ export class DesignNotesForm extends React.Component {
                 return acc;
               }, {})}
               onSubmit={(values, actions) => {
-                this.submitNote(values).then(({ data, status}) => {
+                this.submitNote(values).then(({ data, status }) => {
                   if (status < 400) {
                     if (window) {
                       window.location.reload();
@@ -44,31 +58,47 @@ export class DesignNotesForm extends React.Component {
                 })
               }}
               render={({ errors, status, touched, isSubmitting }) => (
-                <FormikForm>
+                <FormikForm className="design-notes-form">
                   {this.props.properties.map((f) => {
                     if (f === "artStatus") {
                       return (
                         <div key={f}>
-                          <label htmlFor={f}>{f}:{' '}</label>
-                          <Field
-                            component="select"
-                            name={f}
-                          >
-                            <option value="Art In">Art In</option>
-                            <option value="Photo edited, no Camayak">Photo edited, no Camayak</option>
-                            <option value="Waiting for courtesies">Waiting for courtesies</option>
-                          </Field>
+                          <Box sx={{ minWidth: 240, mt: 3 }}>
+                            <FormControl fullWidth>
+                              <InputLabel id="demo-simple-select-standard-label" htmlFor={f}>Art Status</InputLabel>
+                              <Select
+                                labelId="demo-simple-select-standard-label"
+                                id="demo-simple-select-standard"
+                                label="Art Status"
+                                value={this.state.artStatus}
+                                onChange={this.handleChange}
+                                inputProps={{
+                                  name: "artStatus",
+                                  id: "artStatus",
+                                }}
+                              >
+                                <MenuItem value={'Art In'}>Art In</MenuItem>
+                                <MenuItem value={'Photo edited, no Camayak'}>Photo edited, no Camayak</MenuItem>
+                                <MenuItem value={'Waiting for courtesies'}>Waiting for courtesies</MenuItem>
+                              </Select>
+                            </FormControl>
+                          </Box>
                           <ErrorMessage name={f} component="div" />
                         </div>
                       )
                     } else {
                       return (
-                        <div key={f}>
-                          <label htmlFor={f}>{f}:{' '}</label>
-                          <Field
+                        <div key={f} htmlFor={f}>
+                          <TextField
+                            className="home-text-field"
+                            variant="standard"
+                            label={f}
                             type="text"
                             name={f}
                             placeholder={config.designNotes.placeholders[f] || null}
+                            multiline
+                            fullWidth
+                            id={f}
                           />
                           <ErrorMessage name={f} component="div" />
                         </div>
@@ -76,9 +106,9 @@ export class DesignNotesForm extends React.Component {
                     }
                   })}
                   {status && status.msg && <div>{status.msg}</div>}
-                  <button className="primary" type="submit" disabled={isSubmitting}>
-                    <span className="semibold">+</span> Create
-                  </button>
+                  <Button className="create-button" variant="contained" color="primary" type="submit" disabled={isSubmitting} sx={{ mt: 2 }}>
+                    Create
+                  </Button>
                 </FormikForm>
               )}
             />
