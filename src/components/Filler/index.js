bad7e5f59ca5142285234f82c35650582ae24585
angoway <ngoamandauyen@gmail.com>
2025-02-19 02:49:02 -0800

    Filler text styling to look like the Figma

--- src/components/Filler/index.js
+++ src/components/Filler/index.js
@@ -2,12 +2,14 @@ import React, { useState, useRef, useEffect, useMemo } from "react";
 import { LoremIpsum } from "lorem-ipsum";
 import "./style.css";
 import { Copied } from "../Shared/Copied";
+import { FaMinus } from "react-icons/fa6";
+import { FaPlus } from "react-icons/fa6";
 
 export const Filler = () => {
   const [paragraphCount, setParagraphCount] = useState("3");
   const [wordCount, setWordCount] = useState("500");
-  const [copied, setCopied] = useState(false);
-  const [shouldCopy, setShouldCopy] = useState(false); // Flag to trigger copying
+  const [copied, setCopied] = useState(false); // flag for Copied component animation
+  const [shouldCopy, setShouldCopy] = useState(false); // flag for 
   const textareaRef = useRef(null);
 
   function generateText() {
@@ -42,61 +44,108 @@ export const Filler = () => {
 
   const handleWordCountChange = (e) => {
     setWordCount(e.target.value);
-    setCopied(false);
   };
 
   const handleParagraphCountChange = (e) => {
     setParagraphCount(e.target.value);
-    setCopied(false);
   };
+
+  const incOrDecWCPC = (action, type) => {
+    if (action === "increase")
+    {
+      if (type === "WC")
+      {
+        const newValue = parseInt(wordCount) + 50;
+        setWordCount(String(newValue));
+      }
+      else if (type === "PC")
+      {
+        const newValue = parseInt(paragraphCount) + 1;
+        setParagraphCount(String(newValue));
+      }
+    }
+    else if (action === "decrease")
+    {
+      if (type === "WC" && parseInt(wordCount) > 49)
+      {
+        const newValue = parseInt(wordCount) - 50;
+        setWordCount(String(newValue));
+      }
+      else if (type === "PC" && parseInt(paragraphCount) > 0)
+      {
+        const newValue = parseInt(paragraphCount) - 1;
+        setParagraphCount(String(newValue));
+      }
+    }
+  }
 
   useEffect(() => {
     if (shouldCopy && textareaRef.current) {
       navigator.clipboard.writeText(textareaRef.current.value)
-        .then(() => setCopied(true))
+        .then(() => {
+          setCopied(false);
+          setTimeout(() => setCopied(true), 2);
+        })
         .catch(() => console.error("Failed to copy text"));
-
-      setCopied(!copied);
-      setShouldCopy(false); // Reset flag after copying
+      setShouldCopy(false);
     }
-  }, [shouldCopy]); // Runs only when shouldCopy changes to true
+  }, [shouldCopy]);
 
   const text = useMemo(generateText, [wordCount, paragraphCount]);
 
   return (
-    <div className="flex-row">
+    <div>
+      <h1>Filler Text Generator</h1>
       <div className="flex-item">
-        <div>
-          <label htmlFor="wordcount">Word count:</label>
-          <input 
-            onChange={handleWordCountChange}
-            value={wordCount}
-            step="100"
-            type="number"
-            name="wordcount"
-            min="0"
-          />
+        <div className="filler-count">
+          <label htmlFor="wordcount">Word count</label>
+          <div className="filler-number-input">
+            <button onClick={() => incOrDecWCPC("decrease", "WC")} id="filler-button" className="filler-button-minus">
+              <FaMinus className="filler-icon"/>
+            </button>
+            <input 
+              onChange={handleWordCountChange}
+              value={wordCount}
+              step="50"
+              type="number"
+              name="wordcount"
+              min="0"
+            />
+            <button onClick={() => incOrDecWCPC("increase", "WC")} id="filler-button" className="filler-button-plus">
+              <FaPlus className="filler-icon"/>
+            </button>
+          </div>
         </div>
-        <div>
-          <label htmlFor="paragraphs">Paragraphs:</label>
-          <input 
-            onChange={handleParagraphCountChange}
-            value={paragraphCount}
-            type="number"
-            name="paragraphs"
-            min="1"
-          />
+        <div className="filler-count">
+          <label htmlFor="paragraphs">Paragraphs</label>
+          <div className="filler-number-input">
+            <button onClick={() => incOrDecWCPC("decrease", "PC")} id="filler-button" className="filler-button-minus">
+              <FaMinus className="filler-icon"/>
+            </button>
+            <input 
+              onChange={handleParagraphCountChange}
+              value={paragraphCount}
+              type="number"
+              name="paragraphs"
+              min="1"
+            />
+            <button onClick={() => incOrDecWCPC("increase", "PC")} id="filler-button" className="filler-button-plus">
+                <FaPlus className="filler-icon"/>
+            </button> 
+          </div>
         </div>
-        <button onClick={() => setShouldCopy(true)}>Copy</button>
-        {copied && <Copied/>}
       </div>
-      <div className="flex-item">
+      <div className="filler-textbox-and-copy">
         <textarea 
           ref={textareaRef} 
-          style={textareaRef.current ? {height: textareaRef.current.scrollHeight + "px"} : {}} 
+          style={{ height: "55vh", width: "60vw" }} 
           readOnly 
           value={text} 
         />
+        <div>
+            <button id="copy" type="button" onClick={() => setShouldCopy(true)}>Copy</button>
+        </div>
+        {copied && <Copied/>}
       </div>
     </div>
   );
